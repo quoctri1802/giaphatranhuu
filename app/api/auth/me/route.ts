38240627b01fc46918307e7d9ab613aa5cd65c68
quoctri1802 <quoctri1802@gmail.com>
2026-04-24 11:06:54 +0700

    Fix: Force dynamic for API routes to fix Vercel build error

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const authSession = cookies().get('auth_session');
   
